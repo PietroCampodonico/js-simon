b7922427c29e0791a7cc245533756b00124ed202
PietroCampodonico <pietro.campodonico@outlook.it>
2021-04-21 19:54:08 +0200

    esercizio completato

--- myfunctions.js
+++ myfunctions.js
@@ -1,29 +1,51 @@
 //Funzione che genera 5 numeri casuali, li inserisce in un array e li mostra all'utente tramite un alert
+function simonSays() {
 
-function simonSays(arrayNumbers, rNGenerator) {
     var arrayNumbers = [];
 
     for (var i = 0; i < 5; i++) {
 
-        var rNGenerator = Math.ceil(Math.random() * 50);
-        arrayNumbers.push(rNGenerator);
+        var randomNumber = Math.ceil(Math.random() * 50);
+        arrayNumbers.push(randomNumber);
     }
 
-    return console.log(arrayNumbers)
+    alert("Simon says... " + "'" + arrayNumbers.join(" - ") + "'!");
+    return arrayNumbers;
 }
 
+function countdown(generatedNumbers) {
+    var timeSpan = 30;
+    var arrayInput = [];
+    var rememberedNumbers = [];
 
-var timeSpan = 30;
-function countdown(timeSpan) {
-    
     var timer = setInterval(function () {
-    
-        document.getElementById("countdown").innerHTML = timeSpan - 1;
-        timeSpan--
-    
+
+        document.getElementById("countdown").innerHTML = timeSpan - 1 + '" ';
+        timeSpan--;
+
         if (!timeSpan) {
             clearInterval(timer)
+            for (var i = 0; i < 5; i++) {
+                var inputUtente = parseInt(prompt("Simon said..."));
+                if (Number.isNaN(inputUtente)) {
+                    alert("Inserire un numero");
+                    i--;
+
+                } else {
+                    arrayInput.push(inputUtente);
+                }
+            }
+
+            for (var i = 0; i < generatedNumbers.length; i++) {
+
+                if (arrayInput.includes(generatedNumbers[i])) {
+                    rememberedNumbers.push(generatedNumbers[i]);
+                }
+            }
+
+            alert("Hai ricordato " + rememberedNumbers.length + " numero/i! " + "(" + rememberedNumbers.join(" - ") + ")")
+            return rememberedNumbers;
         }
-    
     }, 1000);
 }
+
